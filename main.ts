d5677334d8e64b461c1029eaa7956f1039a49403
lyhdra99 <lyhdra99@gmail.com>
2023-01-19 00:17:25 +0900

    init

--- main.ts
+++ main.ts
@@ -1,9 +1,4 @@
-import Calculator from "./Calculator";
-import Client from "./Client";
-
 console.log("###################### "+new Date().toLocaleTimeString("ko-KR"));
 
-let client = new Client();
-client.main();
 
 console.log("###################### "+new Date().toLocaleTimeString("ko-KR"));
