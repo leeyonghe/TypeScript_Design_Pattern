c5461b17199de3df9250b1292a3704155d258a3d
lyhdra99 <lyhdra99@gmail.com>
2023-01-23 19:39:03 +0900

    init

--- AbstractOperator.ts
+++
@@ -1,8 +0,0 @@
-abstract class AbstractOperator{
-
-    abstract getAnswer(firstNumber : number, secondNumber : number) : number;
-    abstract getDescription() : string;
-
-}
-
-export default AbstractOperator;
--- main.ts
+++ main.ts
@@ -1,21 +1,4 @@
-import AbstractOperation from "./AbstractOperation";
-import AddOperation from "./AddOperation";
-import MultiflyOperation from "./MultiflyOperation";
-import SubstractOperation from "./SubstractOperation";
-
 console.log("###################### "+new Date().toLocaleTimeString("ko-KR"));
-
-
-let firstNumber = 100;
-let secondNumber = 90;
-
-let operations = [new AddOperation(), new SubstractOperation(), new MultiflyOperation()];
-
-for (const operation of operations) {
-    operation.firstNumber = firstNumber;
-    operation.secondNumber = secondNumber;
-    operation.operator();
-}
 
 
 console.log("###################### "+new Date().toLocaleTimeString("ko-KR"));
