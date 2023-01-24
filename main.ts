99951a50df7e1cc341dda884dfabd14e01d5c20f
lyhdra99 <lyhdra99@gmail.com>
2023-01-24 13:46:56 +0900

    init

--- main.ts
+++ main.ts
@@ -1,24 +1,6 @@
-import IntergerOperationFactory from "./IntergerOperationFactory";
-import IntergerOperationProduct from "./IntergerOperationProduct";
-
 console.log("###################### "+new Date().toLocaleTimeString("ko-KR"));
 
 
-
-let firstNumer = "10.2"
-let secondNumber = "20.3";
-
-let operationFactory = new IntergerOperationFactory();
-
-let operationProduct = operationFactory.createOperationProduct();
-
-let firstNumberOperandProduct = operationFactory.createNumberOperandProduct(firstNumer);
-let secondNumberOperandProduct = operationFactory.createNumberOperandProduct(secondNumber); 
-
-operationProduct.firstNumberOperandProduct = firstNumberOperandProduct;
-operationProduct.secondNumberOperandProduct = secondNumberOperandProduct;
-
-operationProduct.print();
 
 
 console.log("###################### "+new Date().toLocaleTimeString("ko-KR"));
