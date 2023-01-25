a3248a0ed4b96838d9f1147697848f3b9fd1fdfa
lyhdra99 <lyhdra99@gmail.com>
2023-01-26 01:38:51 +0900

    init

--- main.ts
+++ main.ts
@@ -1,19 +1,5 @@
-import BaseOperation from "./BaseOperation";
-import RefinedAbastractOperation from "./RefinedAbastrctOperation";
 
 console.log("###################### "+new Date().toLocaleTimeString("ko-KR"));
 
-let firstNumber = 100;
-let secondNumber = 90;
-
-let baseOperation = new BaseOperation();
-
-let operationAbstract = new RefinedAbastractOperation(baseOperation);
-
-operationAbstract.add(firstNumber, secondNumber);
-
-operationAbstract.substract(firstNumber, secondNumber);
-
-operationAbstract.multiply(firstNumber, secondNumber);
 
 console.log("###################### "+new Date().toLocaleTimeString("ko-KR"));
