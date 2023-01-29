c7ba9bd2cb4da72fcad7880852c5d4981e498333
lyhdra99 <lyhdra99@gmail.com>
2023-01-29 17:51:19 +0900

    init

--- ISubject.ts
+++
@@ -1,9 +0,0 @@
-import AbstractOperationObserver from "./AbstractOperationObserver";
-
-interface ISubject{
-    notifyObservers() : void;
-    registerObservers(observer : AbstractOperationObserver): void;
-    removeObservers(observer : AbstractOperationObserver): void;
-}
-
-export default ISubject;
--- main.ts
+++ main.ts
@@ -1,32 +1,3 @@
-import AddOperationObserver from "./AddOperationObserver";
-import DivideOperationObserver from "./DivideOperationObserver";
-import MultiplyOperationObserver from "./MultiplyOperationObserver";
-import OperationSubject from "./OperationSubject";
-import SubstractOperationObserver from "./SubstractOperationObserver";
-
 console.log("###################### "+new Date().toLocaleTimeString("ko-KR"));
-
-let operationSubject = new OperationSubject();
-
-let addObserver = new AddOperationObserver(operationSubject);
-let substractObserver = new SubstractOperationObserver(operationSubject);
-let multiplyObserver = new MultiplyOperationObserver(operationSubject);
-let divideObserver = new DivideOperationObserver(operationSubject);
-
-operationSubject.registerObservers(addObserver);
-operationSubject.registerObservers(substractObserver);
-operationSubject.registerObservers(multiplyObserver);
-operationSubject.registerObservers(divideObserver);
-
-let firstNumber = 100;
-let secondNumber = 90;
-
-operationSubject.setNumber(firstNumber, secondNumber);
-
-firstNumber = 80;
-secondNumber = 20;
-
-operationSubject.setNumber(firstNumber, secondNumber);
-
 
 console.log("###################### "+new Date().toLocaleTimeString("ko-KR"));
