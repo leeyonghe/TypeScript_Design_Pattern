0223f32af428e5bbf27c60aad34dd57d73847951
lyhdra99 <lyhdra99@gmail.com>
2023-01-29 11:52:21 +0900

    init

--- AbstractExpression.ts
+++
@@ -1,9 +0,0 @@
-import CalcContext from "./CalcContext";
-
-abstract class AbstractExpression {
-
-    abstract parse(context : CalcContext) : void;
-    abstract operate() : number;
-
-}
-export default AbstractExpression;
--- main.ts
+++ main.ts
@@ -1,27 +1,3 @@
-import CalcContext from "./CalcContext";
-import CalcExpressionFactory from "./CalcExpressionFactory";
-import CalcToken from "./CalcToken";
-
 console.log("###################### "+new Date().toLocaleTimeString("ko-KR"));
-
-
-
-let text = "ADD(2,12)";
-let calcContext = new CalcContext(text);
-
-let result = 0;
-let currentToken : CalcToken = calcContext.getCurrentTokenAndToNext();
-
-console.log("currentToken token : "+currentToken.token);
-console.log("currentToken type : "+currentToken.type);
-
-let expression = CalcExpressionFactory.getInstance().createExpression(currentToken);
-
-if (expression != null) {
-    expression.parse(calcContext);
-    result = expression.operate();
-}
-
-console.log(text + " = " + result);
 
 console.log("###################### "+new Date().toLocaleTimeString("ko-KR"));
