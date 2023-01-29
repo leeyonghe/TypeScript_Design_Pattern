52b7081a0d7aa66845793e687631820841eb2327
lyhdra99 <lyhdra99@gmail.com>
2023-01-30 00:18:41 +0900

    init

--- IState.ts
+++
@@ -1,8 +0,0 @@
-import IContext from "./IContext";
-
-interface IState {
-
-    action(context : IContext, actionCommand : string) : void;
-
-}
-export default IState;
--- main.ts
+++ main.ts
@@ -1,15 +1,4 @@
-import Client from "./Client";
-
 console.log("###################### "+new Date().toLocaleTimeString("ko-KR"));
 
-let client = new Client();
-
-client.action("1");
-client.action("0");
-client.action("0");
-client.action("+");
-client.action("9");
-client.action("0");
-client.action("=");
 
 console.log("###################### "+new Date().toLocaleTimeString("ko-KR"));
