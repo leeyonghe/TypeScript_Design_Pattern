cb44f84bb7cfdef6df1553efa55a7f655ede1c33
lyhdra99 <lyhdra99@gmail.com>
2023-01-29 15:36:42 +0900

    init

--- main.ts
+++ main.ts
@@ -1,15 +1,4 @@
-import ClientMediator from "./ClientMediator";
-
 console.log("###################### "+new Date().toLocaleTimeString("ko-KR"));
 
-let mediator = new ClientMediator();
-
-let colleague = mediator.addColleague;
-
-colleague.changeFirstNumber(50);
-
-colleague = mediator.substractColleague;
-
-colleague.changeSecondNumber(100);
 
 console.log("###################### "+new Date().toLocaleTimeString("ko-KR"));
