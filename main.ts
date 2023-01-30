243b68846d63ffb183b1c78dafac917a9ae464b0
lyhdra99 <lyhdra99@gmail.com>
2023-01-31 00:29:53 +0900

    init

--- main.ts
+++ main.ts
@@ -1,25 +1,5 @@
-import AddOperation from "./AddOperation";
-import DivideOperation from "./DivideOperation";
-import MultiplyOperation from "./MultiplyOperation";
-import SubstractOperation from "./SubstractOperation";
-
 console.log("###################### "+new Date().toLocaleTimeString("ko-KR"));
 
-let firstNumber = 100;
-let secondNumber = 90;
-
-let operations = [
-    new AddOperation(),
-    new SubstractOperation(),
-    new MultiplyOperation(),
-    new DivideOperation()
-];
-
-for (const operation of operations) {
-    operation.firstNumber = firstNumber;
-    operation.secondNumber = secondNumber;
-    operation.operate();
-}
 
 
 console.log("###################### "+new Date().toLocaleTimeString("ko-KR"));
